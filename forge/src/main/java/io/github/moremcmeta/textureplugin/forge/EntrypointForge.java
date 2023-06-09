e2ec5f9484a8fab1d2c92c89e964bbc0212e185d
soir20 <71418127+soir20@users.noreply.github.com>
2023-06-08 23:00:11 -0400

    Add missing Forge entrypoint

---
+++ forge/src/main/java/io/github/moremcmeta/textureplugin/forge/EntrypointForge.java
@@ -0,0 +1,34 @@
+package io.github.moremcmeta.textureplugin.forge;
+
+import io.github.moremcmeta.textureplugin.ModConstants;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
+
+/**
+ * Mod entrypoint on Forge.
+ * @author soir20
+ */
+@SuppressWarnings("unused")
+@Mod(ModConstants.MOD_ID)
+public class EntrypointForge {
+
+    /**
+     * Serves as mod entrypoint on Forge and tells the server to ignore this mod.
+     */
+    public EntrypointForge() {
+
+        /* Make sure the mod being absent on the other network side does not
+           cause the client to display the server as incompatible. */
+        ModLoadingContext.get().registerExtensionPoint(
+                IExtensionPoint.DisplayTest.class,
+                ()-> new IExtensionPoint.DisplayTest(
+                        () -> NetworkConstants.IGNORESERVERONLY,
+                        (remoteVersion, isServer)-> true
+                )
+        );
+
+    }
+
+}
