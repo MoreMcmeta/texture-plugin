b84dc8227546936647dc32d1b3fb040becfa21f6
soir20 <71418127+soir20@users.noreply.github.com>
2023-06-08 20:43:22 -0400

    Rename packages to textureplugin

--- common/src/main/java/io/github/moremcmeta/textureplugin/ExampleMod.java
+++ common/src/main/java/io/github/moremcmeta/textureplugin/ExampleMod.java
@@ -1,4 +1,4 @@
-package io.github.myorg.myplugin;
+package io.github.moremcmeta.textureplugin;
 
 public class ExampleMod {
     public static final String MOD_ID = "myplugin";
--- forge/src/main/java/io/github/moremcmeta/textureplugin/forge/ExampleModForge.java
+++ forge/src/main/java/io/github/moremcmeta/textureplugin/forge/ExampleModForge.java
@@ -1,6 +1,6 @@
-package io.github.myorg.myplugin.forge;
+package io.github.moremcmeta.textureplugin.forge;
 
-import io.github.myorg.myplugin.ExampleMod;
+import io.github.moremcmeta.textureplugin.ExampleMod;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.IExtensionPoint;
